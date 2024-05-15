66f7f316512137da60a2168c7c326d62c95e7566
SwiperWitty <swiper_witty@qq.com>
2024-05-15 10:06:47 +0800

    gui

--- GUI/caven_gui.c
+++ GUI/caven_gui.c
@@ -11,7 +11,9 @@ void Caven_GUI_draw_pixel_bind(draw_pixel_pFun Fun)
 }
 
 
-
+/*
+    返回值是中间点((X<<8) + Y)
+*/
 int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U16 color)
 {
     int retval = 0;
@@ -40,16 +42,16 @@ int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U
         return retval;
     }
 
-    if (wide % 2)
-    {
-        middle_point = (wide + 1) / 2;
-        middle_point += radius;
-    }
     subzone = percent / 25 + 1;
     f_temp_num = percent % 25;
     f_temp_percent = (f_temp_num / 25) * 90;         // 变成角度
     f_temp_num = (f_temp_percent / 180) * 3.1415926; //
 
+    if (wide % 2)
+    {
+        middle_point = (wide + 1) / 2;
+        middle_point += radius;
+    }
     do
     {
         a = 0;
@@ -99,7 +101,9 @@ int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U
                     if (middle_maxpointx <= show_x) // 取大
                     {
                         middle_maxpointx = show_x;
-                        middle_maxpointy = show_y;
+                        if (middle_maxpointy <= show_y) {
+                            middle_maxpointy = show_y;  // 取大，注意初始值
+                        }
                     }
                 }
             }
@@ -117,7 +121,7 @@ int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U
         {
             x_point = Outside_cos_max + x0;
             middle_maxpointx = ~0;
-            middle_maxpointy = ~0;
+            middle_maxpointy = 0;
             for (int i = 0; i < temp_run; i++)
             {
                 show_x = x0 + pic_array[i][0];
@@ -128,7 +132,9 @@ int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U
                     if (middle_maxpointx >= show_x) // 取小
                     {
                         middle_maxpointx = show_x;
-                        middle_maxpointy = show_y;
+                        if (middle_maxpointy <= show_y) {
+                            middle_maxpointy = show_y;  // 取大，注意初始值
+                        }
                     }
                 }
             }
@@ -157,7 +163,9 @@ int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U
                     if (middle_maxpointx >= show_x) // 取小
                     {
                         middle_maxpointx = show_x;
-                        middle_maxpointy = show_y;
+                        if (middle_maxpointy >= show_y) {
+                            middle_maxpointy = show_y;  // 取小，注意初始值
+                        }
                     }
                 }
             }
@@ -175,7 +183,7 @@ int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U
         {
             x_point = x0 - Outside_cos_max;
             middle_maxpointx = 0;
-            middle_maxpointy = 0;
+            middle_maxpointy = ~0;
             for (int i = 0; i < temp_run; i++)
             {
                 show_x = x0 - pic_array[i][0];
@@ -183,10 +191,12 @@ int Caven_GUI_Draw_Circle(U16 x0, U16 y0, U16 radius, char wide, char percent, U
                 if (show_x <= x_point)
                 {
                     Caven_draw_pixel_pFun(show_x, show_y, color);
-                    if (middle_maxpointx <= show_x) // 取大
+                    if (middle_maxpointx <= show_x)
                     {
-                        middle_maxpointx = show_x;
-                        middle_maxpointy = show_y;
+                        middle_maxpointx = show_x;      // 取大
+                        if (middle_maxpointy >= show_y) {
+                            middle_maxpointy = show_y;  // 取小，注意初始值
+                        }
                     }
                 }
             }
